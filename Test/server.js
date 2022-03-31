1021c66c55ddb2110edd1ed10a8c95afea09d6ae
mitardrazic95 <drazic.mitar@gmail.com>
2022-03-31 21:56:27 +0200

    add cors origin

--- Test/server.js
+++ Test/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 const http = require('http').Server(app);
-const io = require('socket.io')(http);
+const io = require('socket.io')(http, {
+    cors: { origin: "*" }
+});
 
 app.get('/', function(req, res) {
    res.sendfile('index.html');
